866ad22c739e453e0245cc2e8afb189fe514d86c
anna <bohachenko.anna@gmail.com>
2025-05-28 20:12:37 +0200

    tests for registration

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space/',
-    defaultCommandTimeout: 3000,
-    retries: { "runMode": 1, "openMode": 2 },
+    defaultCommandTimeout: 10000,
+    retries: { "runMode": 1, "openMode": 1 },
     video: false,
     screenshotOnRunFailure: true,
     testIsolation: true
