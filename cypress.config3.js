f6d6abec47b1c30a72f2f371ca4b7009ab9a8c5e
anna <bohachenko.anna@gmail.com>
2025-06-02 14:32:09 +0200

    POM for registration tests

---
+++ cypress.config3.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space/',
+    defaultCommandTimeout: 10000,
+    retries: { "runMode": 1, "openMode": 1 },
+    video: false,
+    screenshotOnRunFailure: true,
+    testIsolation: true
+  },
+  env: {
+    AUTH_EMAIL: `bohachenko.anna+test${Date.now()}@gmail.com`,
+    AUTH_PASSWORD: `Qwerty123456`
+  }
+});
--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,71 +1,49 @@
-/*/// <reference types="cypress" />
-import SignInForm from "../pom/forms/SignInForm.js";  
+/// <reference types="cypress" />
+import SignUpForm from "../pom/forms/SignUpForm.js";
 import HomePage from "../pom/pages/HomePage.js";
-
-const email = `bohachenko.anna+test${Date.now()}@gmail.com`;
-const password = `Qwerty123456`;
 
 describe("open registration form", () => {
   beforeEach(() => {
-    cy.visit("/");
-    cy.get("button.hero-descriptor_btn").should("be.visible").click();
+    HomePage.visit();
+    SignUpForm.openSignUpForm();
   });
 
   describe('Field "Name"', () => {
     context("Positive cases", () => {
       it("Name with 2 letters is valid", () => {
-        cy.get("input#signupName").should("be.visible").type("Li").blur();
-        cy.get("input#signupName").should("have.class", "ng-valid");
+        SignUpForm.enterName("Li")  ;
+        SignUpForm.verifyDataIsValid(SignUpForm.nameField);
       });
 
       it("Name with 20 letters is valid", () => {
-        cy.get("input#signupName")
-          .should("be.visible")
-          .type("A".repeat(20))
-          .blur();
-        cy.get("input#signupName").should("have.class", "ng-valid");
+        SignUpForm.enterName("A".repeat(20));
+        SignUpForm.verifyDataIsValid(SignUpForm.nameField);
       });
     });
 
     context("Negative cases", () => {
       it("Name is required", () => {
-        cy.get("input#signupName").focus().blur();
-        cy.get("input#signupName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Name required");
+        SignUpForm.generateErrorByFocusAndBlur(SignUpForm.nameField);
+        SignUpForm.verifyDataIsInvalid(SignUpForm.nameField);
+        SignUpForm.NameRequiredError();
       });
 
       it("Only English letters are allowed", () => {
-        cy.get("input#signupName").type("Анна").blur();
-        cy.get("input#signupName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Name is invalid");
+        SignUpForm.enterName("Анна");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.nameField);
+        SignUpForm.NameIsInvalidError();
       });
 
       it("Name with 1 letter is invalid", () => {
-        cy.get("input#signupName").type("A").blur();
-        cy.get("input#signupName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Name has to be from 2 to 20 characters long");
+        SignUpForm.enterName("A");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.nameField);
+        SignUpForm.NameLengthError();
       });
 
       it("Name with 21 letters is invalid", () => {
-        cy.get("input#signupName").type("A".repeat(21)).blur();
-        cy.get("input#signupName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Name has to be from 2 to 20 characters long");
+        SignUpForm.enterName("A".repeat(21));
+        SignUpForm.verifyDataIsInvalid(SignUpForm.nameField);
+        SignUpForm.NameLengthError();
       });
     });
   });
@@ -73,55 +51,39 @@ describe("open registration form", () => {
   describe('Field "Last Name"', () => {
     context("Positive cases", () => {
       it("Last Name with 2 letters is valid", () => {
-        cy.get("input#signupLastName").type("Pi").blur();
-        cy.get("input#signupLastName").should("have.class", "ng-valid");
+        SignUpForm.enterLastName("Pi")  ;
+        SignUpForm.verifyDataIsValid(SignUpForm.lastNameField);
       });
 
       it("Last Name with 20 letters is valid", () => {
-        cy.get("input#signupLastName").type("A".repeat(20)).blur();
-        cy.get("input#signupLastName").should("have.class", "ng-valid");
+        SignUpForm.enterLastName("A".repeat(20));
+        SignUpForm.verifyDataIsValid(SignUpForm.lastNameField);
       });
     });
 
     context("Negative cases", () => {
       it("Last Name is required", () => {
-        cy.get("input#signupLastName").focus().blur();
-        cy.get("input#signupLastName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Last name required");
+        SignUpForm.generateErrorByFocusAndBlur(SignUpForm.lastNameField);
+        SignUpForm.verifyDataIsInvalid(SignUpForm.lastNameField);
+        SignUpForm.LastNameRequiredError();
       });
 
       it("Only English letters are allowed", () => {
-        cy.get("input#signupLastName").type("Анна").blur();
-        cy.get("input#signupLastName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Last name is invalid");
+        SignUpForm.enterLastName("Анна");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.lastNameField);
+        SignUpForm.LastNameIsInvalidError();
       });
 
       it("Last Name with 1 letter is invalid", () => {
-        cy.get("input#signupLastName").type("A").blur();
-        cy.get("input#signupLastName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Last name has to be from 2 to 20 characters long");
+        SignUpForm.enterLastName("A");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.lastNameField);
+        SignUpForm.LastNameLengthError();
       });
 
       it("Last Name with 21 letters is invalid", () => {
-        cy.get("input#signupLastName").type("A".repeat(21)).blur();
-        cy.get("input#signupLastName")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Last name has to be from 2 to 20 characters long");
+        SignUpForm.enterLastName("A".repeat(21));
+        SignUpForm.verifyDataIsInvalid(SignUpForm.lastNameField);
+        SignUpForm.LastNameLengthError();
       });
     });
   });
@@ -129,30 +91,22 @@ describe("open registration form", () => {
   describe('Field "Email"', () => {
     context("Positive cases", () => {
       it("Email is valid", () => {
-        cy.get("input#signupEmail").type(`${email}`).blur();
-        cy.get("input#signupEmail").should("have.class", "ng-valid");
+        SignUpForm.enterEmail(Cypress.env('AUTH_EMAIL'));
+        SignUpForm.verifyDataIsValid(SignUpForm.emailField);
       });
     });
 
     context("Negative cases", () => {
       it("Email is required", () => {
-        cy.get("input#signupEmail").focus().blur();
-        cy.get("input#signupEmail")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Email required");
+        SignUpForm.generateErrorByFocusAndBlur(SignUpForm.emailField);
+        SignUpForm.verifyDataIsInvalid(SignUpForm.emailField);
+        SignUpForm.EmailRequiredError();
       });
 
       it("Email is invalid", () => {
-        cy.get("input#signupEmail").type("anna.bohachenko").blur();
-        cy.get("input#signupEmail")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Email is incorrect");
+        SignUpForm.enterEmail("anna.bohachenko");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.emailField);
+        SignUpForm.EmailIsInvalidError();
       });
     });
   });
@@ -160,90 +114,51 @@ describe("open registration form", () => {
   describe('Field "Password"', () => {
     context("Positive cases", () => {
       it("Password with 15 letters is valid", () => {
-        cy.get("input#signupPassword").type("Qwerty123456789").blur();
-        cy.get("input#signupPassword").should("have.class", "ng-valid");
+        SignUpForm.enterPassword("Qwerty123456789");
+        SignUpForm.verifyDataIsValid(SignUpForm.passwordField);
       });
 
       it("Password with 8 letters is valid", () => {
-        cy.get("input#signupPassword").type("Qwerty12").blur();
-        cy.get("input#signupPassword").should("have.class", "ng-valid");
+        SignUpForm.enterPassword("Qwerty12");
+        SignUpForm.verifyDataIsValid(SignUpForm.passwordField);
       });
     });
 
     context("Negative cases", () => {
       it("Password is required", () => {
-        cy.get("input#signupPassword").focus().blur();
-        cy.get("input#signupPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Password required");
+        SignUpForm.generateErrorByFocusAndBlur(SignUpForm.passwordField);
+        SignUpForm.verifyDataIsInvalid(SignUpForm.passwordField);
+        SignUpForm.PasswordRequiredError();
       });
 
       it("Password with 7 letters is invalid", () => {
-        cy.get("input#signupPassword").type("Qwerty1").blur();
-        cy.get("input#signupPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and(
-            "contain",
-            "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-          );
+        SignUpForm.enterPassword("Qwerty1");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.passwordField);
+        SignUpForm.PasswordRequirementsError();
       });
 
       it("Password with 16 letters is invalid", () => {
-        cy.get("input#signupPassword").type("Qwerty1234567890").blur();
-        cy.get("input#signupPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and(
-            "contain",
-            "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-          );
+        SignUpForm.enterPassword("Qwerty1234567890");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.passwordField);
+        SignUpForm.PasswordRequirementsError();
       });
 
       it("1 integer is required", () => {
-        cy.get("input#signupPassword").type("NoInteger").blur();
-        cy.get("input#signupPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and(
-            "contain",
-            "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-          );
+        SignUpForm.enterPassword("NoInteger");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.passwordField);
+        SignUpForm.PasswordRequirementsError();
       });
 
       it("1 capital letter is required", () => {
-        cy.get("input#signupPassword").type("nocapital1etter").blur();
-        cy.get("input#signupPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and(
-            "contain",
-            "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-          );
+        SignUpForm.enterPassword("nocapital1etter");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.passwordField);
+        SignUpForm.PasswordRequirementsError();
       });
 
       it("1 small letter is required", () => {
-        cy.get("input#signupPassword").type("NOSMALLLETTER1").blur();
-        cy.get("input#signupPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and(
-            "contain",
-            "Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter"
-          );
+        SignUpForm.enterPassword("NOSMALLLETTER1");
+        SignUpForm.verifyDataIsInvalid(SignUpForm.passwordField);
+        SignUpForm.PasswordRequirementsError();
       });
     });
   });
@@ -251,32 +166,24 @@ describe("open registration form", () => {
   describe('Field "Re-enter password"', () => {
     context("Positive cases", () => {
       it("Re-enter password is valid", () => {
-        cy.get("input#signupPassword").type(`${password}`);
-        cy.get("input#signupRepeatPassword").type(`${password}`).blur();
-        cy.get("input#signupRepeatPassword").should("have.class", "ng-valid");
+        SignUpForm.enterPassword(Cypress.env('AUTH_PASSWORD'));
+        SignUpForm.enterRepeatPassword(Cypress.env('AUTH_PASSWORD'));
+        SignUpForm.verifyDataIsValid(SignUpForm.repeatPasswordField);
       });
     });
 
     context("Negative cases", () => {
       it("Re-enter password is required", () => {
-        cy.get("input#signupRepeatPassword").focus().blur();
-        cy.get("input#signupRepeatPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "ng-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Re-enter password required");
+        SignUpForm.generateErrorByFocusAndBlur(SignUpForm.repeatPasswordField);
+        SignUpForm.verifyDataIsInvalid(SignUpForm.repeatPasswordField);
+        SignUpForm.ReEnterPasswordRequiredError();
       });
 
       it("Re-enter password is invalid", () => {
-        cy.get("input#signupPassword").type(`${password}`);
-        cy.get("input#signupRepeatPassword").type("12345Qwerty").blur();
-        cy.get("input#signupRepeatPassword")
-          .should("have.css", "border-color", "rgb(220, 53, 69)")
-          .and("have.class", "is-invalid");
-        cy.get(".invalid-feedback")
-          .should("be.visible")
-          .and("contain", "Passwords do not match");
+        SignUpForm.enterPassword(Cypress.env('AUTH_PASSWORD'));
+        SignUpForm.enterRepeatPassword("12345Qwerty");
+        SignUpForm.verifyPasswordIsInvalid(SignUpForm.repeatPasswordField);
+        SignUpForm.ReEnterPasswordMatchError();
       });
     });
   });
@@ -284,31 +191,24 @@ describe("open registration form", () => {
   describe("Sending form", () => {
     context("Positive cases", () => {
       it("New User is created when data is correct", () => {
-        cy.get("input#signupName").type("Anna");
-        cy.get("input#signupLastName").type("Bohachenko");
-        cy.get("input#signupEmail").type(`${email}`);
-        cy.get("input#signupPassword").type(`${password}`);
-        cy.get("input#signupRepeatPassword").type(`${password}`).blur();
-        cy.get("div.modal-footer>button.btn-primary").click();
-        cy.get("div.alert-success")
-          .should("be.visible")
-          .and("contain", "Registration complete");
+        SignUpForm.signUpWithCredentials("Anna", "Bohachenko", Cypress.env('AUTH_EMAIL'), Cypress.env('AUTH_PASSWORD'));
+        SignUpForm.verifySuccessMessage
       });
     });
 
     context("Negative cases", () => {
       it("[Register] button is disabled when form is empty", () => {
-        cy.get("button.btn-primary").should("be.disabled");
+        SignUpForm.ConfirmSignUpButtonIsDisabled();
       });
       it("[Register] button is disabled when data is incorrect", () => {
-        cy.get("input#signupName").type("Anna");
-        cy.get("input#signupLastName").type("Bohachenko");
-        cy.get("input#signupEmail").type(`${email}`);
-        cy.get("input#signupPassword").type(`${password}`);
-        cy.get("input#signupRepeatPassword").type(`qwerty54321`).blur();
-        cy.get("button.btn-primary").should("be.disabled");
+        SignUpForm.enterName("Anna");
+        SignUpForm.enterLastName("Bohachenko");
+        SignUpForm.enterEmail(Cypress.env('AUTH_EMAIL'));
+        SignUpForm.enterPassword(Cypress.env('AUTH_PASSWORD'));
+        SignUpForm.enterRepeatPassword(`Qwerty54321`);
+        SignUpForm.ReEnterPasswordMatchError();
+        SignUpForm.ConfirmSignUpButtonIsDisabled();
       });
     });
   });
 });
-*/
